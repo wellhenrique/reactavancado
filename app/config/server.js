9e9de31a8cb832ac39318152fff5332c865085dc
wellhenrique <welissonh80@gmail.com>
2022-09-12 19:59:21 -0300

    [feat]: app created

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a42544bf6c0528238b0891b42eef326'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '12df0683-97b1-4d99-b5b0-de63353071b1'
+};
